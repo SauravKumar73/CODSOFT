21c2a96aa3f9dc62e2d86bc186a4b4fad93142de
Saurav Kumar <107035562+SauravKumar73@users.noreply.github.com>
2023-08-13 23:56:43 +0530

    Add files via upload

---
+++ TODO.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add_task():
+    task = task_entry.get()
+    if task:
+        task_list.insert(tk.END, task)
+        task_entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Warning", "Please enter a task.")
+
+def remove_task():
+    selected_task = task_list.curselection()
+    if selected_task:
+        task_list.delete(selected_task)
+    else:
+        messagebox.showwarning("Warning", "Please select a task to remove.")
+
+def clear_tasks():
+    task_list.delete(0, tk.END)
+
+
+app = tk.Tk()
+app.title("To-Do List Application")
+
+
+task_entry = tk.Entry(app, width=40)
+task_entry.pack(pady=10)
+
+add_button = tk.Button(app, text="Add Task", command=add_task)
+add_button.pack()
+
+remove_button = tk.Button(app, text="Remove Task", command=remove_task)
+remove_button.pack()
+
+clear_button = tk.Button(app, text="Clear All Tasks", command=clear_tasks)
+clear_button.pack()
+
+task_list = tk.Listbox(app, width=40)
+task_list.pack(pady=10)
+
+app.mainloop()
